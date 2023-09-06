0ab325124fec25723e0ec9058906722b66520234
ykzZa <n.hryshaienko@gmail.com>
2023-09-06 19:45:30 +0300

    Action bar in activity

--- app/src/main/java/dev/ykzza/giphytest/presentation/MainActivity.kt
+++ app/src/main/java/dev/ykzza/giphytest/presentation/MainActivity.kt
@@ -2,16 +2,30 @@ package dev.ykzza.giphytest.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import dev.ykzza.giphytest.R
 import dev.ykzza.giphytest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_container) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        setupActionBarWithNavController(navController)
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
