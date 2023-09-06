89dfa1f9e536011a236507f9237265a2320d555e
ykzZa <n.hryshaienko@gmail.com>
2023-09-06 19:48:06 +0300

    Gif domain update

--- app/src/main/java/dev/ykzza/giphytest/domain/Gif.kt
+++ app/src/main/java/dev/ykzza/giphytest/domain/Gif.kt
@@ -2,8 +2,6 @@ package dev.ykzza.giphytest.domain
 
 data class Gif(
     val id: String,
-    val title: String,
-    val username: String,
     val originalUrl: String,
     val fixedHeightUrl: String
 )
